2c575792ca4f99bbb62d3fdeef8ae34d24a7c2f1
yempty0 <parkyb0523@naver.com>
2026-07-16 17:16:28 +0900

    feat: FE-12 인증 가드 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,30 +1,30 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import HomePage from '@/pages/HomePage'
-import StockDetailPage from '@/pages/StockDetailPage'
-import PortfolioPage from '@/pages/PortfolioPage'
-import LoginPage from '@/pages/LoginPage'
-import SignupPage from '@/pages/SignupPage'
-import MainLayout from '@/layouts/MainLayout'
-import PrivateRoute from './PrivateRoute'
+import HomePage from "@/pages/HomePage";
+import StockDetailPage from "@/pages/StockDetailPage";
+import PortfolioPage from "@/pages/PortfolioPage";
+import LoginPage from "@/pages/LoginPage";
+import SignupPage from "@/pages/SignupPage";
+import MainLayout from "@/layouts/MainLayout";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
       {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/stocks/:ticker',
-        element: <StockDetailPage />,
-      },
-      {
         element: <PrivateRoute />,
         children: [
           {
-            path: '/portfolio',
+            path: "/",
+            element: <HomePage />,
+          },
+          {
+            path: "/stocks/:ticker",
+            element: <StockDetailPage />,
+          },
+          {
+            path: "/portfolio",
             element: <PortfolioPage />,
           },
         ],
@@ -32,13 +32,13 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage />,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <SignupPage />,
   },
-])
+]);
 
-export default router
+export default router;
